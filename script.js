685ac56620c927aa782243273c2a89d1f896c29f
xyeison2010 <leonardohuacahidalgo@gmail.com>
2022-12-10 18:20:09 -0500

    gallery

---
+++ script.js
@@ -0,0 +1,28 @@
+// jQuery
+/* $(function() {
+    $('.panel').on('mouseover', function () {
+        const $this = $(this);
+        const $panel = $('.panel');
+
+        $panel.removeClass('active');
+        $this.toggleClass('active');
+    });
+}); */
+
+// Vanilla Javascript
+
+const panels = document.querySelectorAll('.panel');
+
+function displayPanel() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    });
+
+    this.classList.toggle('active');
+}
+
+panels.forEach((panel) => {
+    panel.addEventListener('mouseover', displayPanel);
+});
+
+
